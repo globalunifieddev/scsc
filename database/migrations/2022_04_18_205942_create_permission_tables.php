decf6ab5c536319e46425d2c7f1182d86a37d2d7
TK <tasiukwaplong@gmail.com>
2024-02-28 00:48:25 +0100

    FX: duplicate PK in table

--- database/migrations/2022_04_18_205942_create_permission_tables.php
+++ database/migrations/2022_04_18_205942_create_permission_tables.php
@@ -50,7 +50,7 @@ class CreatePermissionTables extends Migration
             }
         });
 
-        Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
+        /*Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
             $table->unsignedBigInteger(PermissionRegistrar::$pivotPermission);
 
             $table->string('model_type');
@@ -72,7 +72,40 @@ class CreatePermissionTables extends Migration
                     'model_has_permissions_permission_model_type_primary');
             }
 
+        });*/
+        Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
+            $table->unsignedBigInteger(PermissionRegistrar::$pivotPermission);
+        
+            $table->string('model_type');
+            $table->unsignedBigInteger($columnNames['model_morph_key']);
+            $table->index([$columnNames['model_morph_key'], 'model_type'], 'model_has_permissions_model_id_model_type_index');
+        
+            $table->foreign(PermissionRegistrar::$pivotPermission)
+                ->references('id')
+                ->on($tableNames['permissions'])
+                ->onDelete('cascade');
+        
+            if ($teams) {
+                $table->unsignedBigInteger($columnNames['team_foreign_key']);
+                $table->index($columnNames['team_foreign_key'], 'model_has_permissions_team_foreign_key_index');
+        
+                // Define composite primary key for teams scenario
+                $table->primary([
+                    $columnNames['team_foreign_key'],
+                    PermissionRegistrar::$pivotPermission,
+                    $columnNames['model_morph_key'],
+                    'model_type'
+                ], 'model_has_permissions_permission_model_type_primary');
+            } else {
+                // Define composite primary key for non-teams scenario
+                $table->primary([
+                    PermissionRegistrar::$pivotPermission,
+                    $columnNames['model_morph_key'],
+                    'model_type'
+                ], 'model_has_permissions_permission_model_type_primary');
+            }
         });
+        
 
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames, $columnNames, $teams) {
             $table->id();
